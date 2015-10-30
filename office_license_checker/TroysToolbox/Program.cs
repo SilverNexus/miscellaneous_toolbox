929cc8dd2db56919f85ef6c3be60ea7309fc608d
SilverNexus <a_daniel.hawkins@yahoo.com>
2015-10-30 13:00:09 -0400

    Add files for office_license_matcher to the repository.

---
+++ office_license_checker/TroysToolbox/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace TroysToolbox
+{
+    class Program
+    {
+        static void ShowOfficeStatus(string foundAt)
+        {
+            // Create a proccess to run the script
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.FileName = "cscript";
+            processInfo.Arguments = "\"" + foundAt + "\" /dstatus";
+            processInfo.RedirectStandardOutput = true;
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+
+            Process proc = Process.Start(processInfo);
+            proc.WaitForExit();
+
+            Console.WriteLine(proc.StandardOutput.ReadToEnd());
+        }
+
+        static void Main(string[] args)
+        {
+            bool found = false;
+            if (System.Environment.Is64BitOperatingSystem)
+            {
+#if DEBUGGING
+                Console.WriteLine("64-bit");
+#endif
+                string[] officePlaces64 = Directory.GetDirectories(@"C://Program Files/Microsoft Office/", "Office*");
+                string[] officePlaces32 = Directory.GetDirectories(@"C://Program Files (x86)/Microsoft Office/", "Office*");
+#if DEBUGGING
+                Console.WriteLine("64-bit office:");
+#endif
+                string foundAt = "";
+                foreach (String str in officePlaces64)
+                {
+#if DEBUGGING
+                    Console.WriteLine(str);
+#endif
+                    string[] vbs = Directory.GetFiles(str, "ospp.vbs");
+                    if (vbs.Length != 0)
+                    {
+                        found = true;
+                        foundAt = str + "/ospp.vbs";
+                    }
+                }
+#if DEBUGGING
+                Console.WriteLine("32-bit office:");
+#endif
+                foreach (String str in officePlaces32)
+                {
+#if DEBUGGING
+                    Console.WriteLine(str);
+#endif
+                    string[] vbs = Directory.GetFiles(str, "ospp.vbs");
+                    if (vbs.Length != 0)
+                    {
+                        found = true;
+                        foundAt = str + "/ospp.vbs";
+                    }
+                }
+                if (!found)
+                {
+                    Console.WriteLine("Could not find ospp.vbs.");
+                }
+                else
+                {
+                    ShowOfficeStatus(foundAt);
+                }
+            }
+            else
+            {
+#if DEBUGGING
+                Console.WriteLine("32-bit");
+#endif
+                string[] officePlaces = Directory.GetDirectories(@"C://Program Files/Microsoft Office/", "Office*");
+#if DEBUGGING
+                Console.WriteLine("32-bit office:");
+#endif
+                string foundAt = "";
+                foreach (String str in officePlaces)
+                {
+#if DEBUGGING
+                    Console.WriteLine(str);
+#endif
+                    string[] vbs = Directory.GetFiles(str, "ospp.vbs");
+                    if (vbs.Length != 0)
+                    {
+                        found = true;
+                        foundAt = str + "/ospp.vbs";
+                    }
+                }
+                if (!found)
+                {
+                    Console.WriteLine("Could not find ospp.vbs.");
+                }
+                else
+                {
+                    ShowOfficeStatus(foundAt);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
